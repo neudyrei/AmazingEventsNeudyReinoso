2ff2fdbcdccdf3c969ecf70378f801453a4a3f18
neudyrei <cocinaconneudy@gmail.com>
2023-10-13 20:59:56 -0500

    Se creo funcion para Filtrar por fechay se le dieron nuevos estilos

---
+++ UpcomingEvents.js
@@ -0,0 +1,55 @@
+const backgroundBody = document.getElementById("body-index")
+backgroundBody.style.backgroundColor = ("#e4c1f9");
+
+const backgroundHeader = document.getElementById("header-index")
+backgroundHeader.style.backgroundColor = ("#ff99c8");
+
+const backgroundFooter = document.getElementById("footer-index")
+backgroundFooter.style.backgroundColor = ("#ff99c8");
+
+
+
+
+const arrayEvents = data.events;
+const arrayPastEvents = []
+const currentDate = ( "2023-01-01")
+
+for (let event of arrayEvents){
+    if(event.date > currentDate){
+        arrayPastEvents.push(event)
+    }
+}
+
+// console.log(arrayPastEvents)
+const contenedor = document.getElementById("cont-cards");
+
+
+
+function boxCards(arrayEvents) {
+    for (let event of arrayEvents) {
+        const card = document.createElement("div")
+
+        card.classList.add("card")
+        card.style.width = "18rem"
+        card.style.backgroundColor = ("#3c096c")
+        card.style.color = ("white")
+        card.innerHTML = `
+
+        <img src="${event.image}" class="card-img-top h-50" 
+        alt="Collectivities Party">
+        <div class="card-body">
+        <h5 class="card-tittle"> ${event.name}</h5>
+        <p class="card-text text-truncate">${event.description}</p>
+        <p class"text-start d-flex">Date: ${event.date}</p>
+        <div class="d-flex justify-content-between">
+        <p class"text-start d-flex"> $ ${event.price}</p>
+        <a href="#" class="btn btn-outline-info">Details </a>
+        </div>
+        </div>
+        `
+        contenedor.appendChild(card)
+    }
+ }
+ console.log(arrayPastEvents)
+
+boxCards(arrayPastEvents)
--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+const backgroundBody = document.getElementById("body-index")
+backgroundBody.style.backgroundColor= ("#e4c1f9");
+
+const backgroundHeader = document.getElementById("header-index")
+backgroundHeader.style.backgroundColor= ("#ff99c8");
+
+const backgroundFooter = document.getElementById("footer-index")
+backgroundFooter.style.backgroundColor= ("#ff99c8");
+
 const arrayEvents = data.events;
 const contenedor = document.getElementById("cont-cards");
 console.log(arrayEvents)
@@ -7,31 +16,22 @@ for (let event of arrayEvents) {
 
     card.classList.add("card")
     card.style.width = "18rem"
+    card.style.backgroundColor= ("#3c096c")
+    card.style.color= ("white")
     card.innerHTML = `
 
     <img src="${event.image}" class="card-img-top h-50" 
      alt="Collectivities Party">
      <div class="card-body">
     <h5 class="card-tittle"> ${event.name}</h5>
-    <p class"card-text text-truncate">${event.description}</p>
+    <p class="card-text text-truncate">${event.description}</p>
+    <p class"text-start d-flex">Date: ${event.date}</p>
         <div class="d-flex justify-content-between">
             <p class"text-start d-flex"> $ ${event.price}</p>
-            <a href="#" class="btn btn-outline-danger">Details </a>
+            <a href="#" class="btn btn-outline-info">Details </a>
         </div>
     </div>
     `
     contenedor.appendChild(card)
 }
 
-// {/* <div class="d-flex flex-wrap justify-content-evenly">
-// <div class="card my-3" style="width: 18rem;">
-//     <img src="./Recursos_Amazing_Events_Task_1/music_concert.jpg" class="card-img-top" alt="music concert">
-//     <div class="card-body">
-//         <h5 class="card-title">Metallica in concert</h5>
-//         <p class="card-text text-truncate">The only concert of the most emblematic band in the world</p>
-//         <div class="d-flex justify-content-between">
-//             <p> $150</p>
-//             <a href="#" class="btn btn-outline-danger">Details </a>
-//         </div>
-//     </div>
-// </div> */}
