5da45126646fe7c8c386883c6a672c9139e5724c
neudyrei <cocinaconneudy@gmail.com>
2023-10-29 22:35:19 -0500

    modificacion de funcion de details

--- details.js
+++ details.js
@@ -41,7 +41,7 @@ function crearCard (event) {
                         <li class="list-group-item"><small class="text-body-secondary">Category: ${event.category}</small></li>
                         <li class="list-group-item"><small class="text-body-secondary">Place: ${event.place}</small></li>
                         <li class="list-group-item"><small class="text-body-secondary">Capacity: ${event.capacity}</small></li>
-                        <li class="list-group-item"><small class="text-body-secondary">Assistance: ${event.assitance}</small></li>
+                        <li class="list-group-item"><small class="text-body-secondary">${event.assistance==null?"Estimate: "+ event.estimate: "Assistance:"+ event.assistance}</small></li>
                         <li class="list-group-item"><small class="text-body-secondary">Price: ${event.price}</small></li>
                     </ul>
                 </div>
@@ -53,3 +53,38 @@ function crearCard (event) {
 }
 
  crearCard(eventEncontrado)
+
+
+//  const consultaURL = new URLSearchParams(window.location.search)
+// const id = consultaURL.get("_id")
+// const usuarios = data.events
+// const evento = usuarios.find(evento => evento._id === id)
+// const contenedorDetails = document.getElementById("contenedorDetails")
+
+// function funcionCards() {
+//     contenedorDetails.classList.add('container', 'shadow', 'd-flex');
+//     const creadorCards = document.createElement("div")
+//     creadorCards.classList.add('card');
+//     creadorCards.innerHTML = 
+//         <div class="row no-gutters">
+//             <div class="col-md-6">
+//                 <img src="${evento.image}" class="card-img h-100 img-fluid"
+//                 alt="Food Fair">
+//             </div>
+//         <div class="col-md-6">
+//         <div class="card-body">
+//             <h5 class="card-title"> ${evento.name}</h5>
+//             <p class="card-text"> ${evento.description}</p>
+//             <p class="card-text"><strong>Date:</strong> ${evento.date}</p>
+//             <p class="card-text"><strong>Category:</strong> ${evento.category}</p>
+//             <p class="card-text"><strong>Place:</strong> ${evento.place}</p>
+//             <p class="card-text"><strong>Capacity:</strong> ${evento.capacity}</p>
+//             <p class="card-text"> ${evento.assistance==null?"<strong>Estimate: </strong>"+evento.estimate:"<strong>Assistance: </strong>"+evento.assistance}</p>
+//             <p class="card-text"><strong>Price:</strong> $ ${evento.price}</p>
+//             </div>
+//         </div>
+//         </div>
+//         contenedorDetails.appendChild(creadorCards);
+// }
+
+// funcionCards (evento)
